bae8bcf1a72a64bb8d860f9304ea7c9d7ac9e90c
vt-d <poonadiscord@gmail.com>
2024-06-02 23:46:05 +0530

    127

--- script.lua
+++ script.lua
@@ -15,106 +15,106 @@ local delete_done = get("done-3")
 local result = get("result")
 
 coroutine.wrap(function()
-    local res = fetch({
-        url = "https://api.buss.lol/tlds",
-        method = "GET",
-        headers = { ["Content-Type"] = "application/json" },
-    })
+	local res = fetch({
+		url = "http://127.0.0.1:3000/tlds",
+		method = "GET",
+		headers = { ["Content-Type"] = "application/json" },
+	})
 
-    local tld_list = table.concat(res, ", ")
-    get("tlds").set_content("Available TLDs: " .. tld_list)
+	local tld_list = table.concat(res, ", ")
+	get("tlds").set_content("Available TLDs: " .. tld_list)
 end)()
 
 function fetch_dns()
-    local body = "{"
-    .. '"tld": "'
-    .. publish_tld.get_content()
-    .. '", '
-    .. '"name": "'
-    .. publish_domain.get_content()
-    .. '", '
-    .. '"ip": "'
-    .. publish_ip.get_content()
-    .. '" }'
+	local body = "{"
+	    .. '"tld": "'
+	    .. publish_tld.get_content()
+	    .. '", '
+	    .. '"name": "'
+	    .. publish_domain.get_content()
+	    .. '", '
+	    .. '"ip": "'
+	    .. publish_ip.get_content()
+	    .. '" }'
 
-    print(body)
-    local res = fetch({
-        url = "https://api.buss.lol/domain",
-        method = "POST",
-        headers = { ["Content-Type"] = "application/json" },
-        body = body,
-    })
-   
-    return res
+	print(body)
+	local res = fetch({
+		url = "http://127.0.0.1:3000/domain",
+		method = "POST",
+		headers = { ["Content-Type"] = "application/json" },
+		body = body,
+	})
+
+	return res
 end
 
 publish_done.on_click(function()
-    local res = fetch_dns()
+	local res = fetch_dns()
 
-    if res["secret_key"] then
-        result.set_content("Congrats! Your key: " .. res["secret_key"] .. "\nPLEASE SAVE IT.")
-    else
-        result.set_content(res.status .. ": " .. res.content)
-    end
+	if res["secret_key"] then
+		result.set_content("Congrats! Your key: " .. res["secret_key"] .. "\nPLEASE SAVE IT.")
+	else
+		result.set_content(res.status .. ": " .. res.content)
+	end
 end)
 
 update_done.on_click(function()
-    local body = "{"
-        .. '"ip": "'
-        .. update_ip.get_content()
-        .. '"'
-        .. "}"
+	local body = "{"
+	    .. '"ip": "'
+	    .. update_ip.get_content()
+	    .. '"'
+	    .. "}"
 
-    local res = fetch({
-        url = "https://api.buss.lol/domain/" .. update_key.get_content(),
-        method = "PUT",
-        headers = { ["Content-Type"] = "application/json" },
-        body = body,
-    })
+	local res = fetch({
+		url = "http://127.0.0.1:3000/domain/" .. update_key.get_content(),
+		method = "PUT",
+		headers = { ["Content-Type"] = "application/json" },
+		body = body,
+	})
 
-    print(res)
+	print(res)
 
-    if res and res.status then
-        if res.status == 429 then
-            result.set_content("Failed due to ratelimit.")
-        elseif res.status == 404 then
-            result.set_content("Failed due to: domain not found.")
-        elseif res.status == 400 then
-            result.set_content("Failed due to: invalid body.\nMake sure all fields are completed")
-        elseif res.status == 200 then
-            result.set_content("Success!")
-        else
-            result.set_content("Failed due to error: " .. res.status)
-        end
-    elseif res and res.ip then
-        result.set_content("Success!")
-    else
-        result.set_content("Failed due to unknown error.")
-    end
+	if res and res.status then
+		if res.status == 429 then
+			result.set_content("Failed due to ratelimit.")
+		elseif res.status == 404 then
+			result.set_content("Failed due to: domain not found.")
+		elseif res.status == 400 then
+			result.set_content("Failed due to: invalid body.\nMake sure all fields are completed")
+		elseif res.status == 200 then
+			result.set_content("Success!")
+		else
+			result.set_content("Failed due to error: " .. res.status)
+		end
+	elseif res and res.ip then
+		result.set_content("Success!")
+	else
+		result.set_content("Failed due to unknown error.")
+	end
 end)
 
 delete_done.on_click(function()
-    local res = fetch({
-        url = "https://api.buss.lol/domain/" .. delete_key.get_content(),
-        method = "DELETE",
-        headers = { ["Content-Type"] = "application/json" },
-    })
+	local res = fetch({
+		url = "http://127.0.0.1:3000/domain/" .. delete_key.get_content(),
+		method = "DELETE",
+		headers = { ["Content-Type"] = "application/json" },
+	})
 
-    print(res)
+	print(res)
 
-    if res and res.status then
-        if res.status == 429 then
-            result.set_content("Failed due to ratelimit.")
-        elseif res.status == 404 then
-            result.set_content("Failed due to: domain not found.")
-        elseif res.status == 200 then
-            result.set_content("Success!")
-        else
-            result.set_content("Failed due to error: " .. res.status)
-        end
-    elseif res and res.secret_key then
-        result.set_content("Success!")
-    else
-        result.set_content("Failed due to unknown error.")
-    end
+	if res and res.status then
+		if res.status == 429 then
+			result.set_content("Failed due to ratelimit.")
+		elseif res.status == 404 then
+			result.set_content("Failed due to: domain not found.")
+		elseif res.status == 200 then
+			result.set_content("Success!")
+		else
+			result.set_content("Failed due to error: " .. res.status)
+		end
+	elseif res and res.secret_key then
+		result.set_content("Success!")
+	else
+		result.set_content("Failed due to unknown error.")
+	end
 end)
